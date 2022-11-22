5c9453604dc1ed41378d0f26178d579f788ccc31
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2022-11-22 20:36:37 +0530

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,43 @@
+/*
+Task
+In this challenge, you must read 3 integers from stdin and then print them to stdout. Each integer must be printed on a new line.
+To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+Input Format
+
+There are 3 lines of input, and each line contains a single integer.
+
+Sample Input
+
+42
+100
+125
+Sample Output
+
+42
+100
+125
+
+Solution - 
+*/
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int b = scan.nextInt();
+        int a = scan.nextInt();
+        int c = scan.nextInt(); 
+        scan.close();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
