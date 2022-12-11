8fd59ee2fa9356aa6c060a497a1abbd99de87a43
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2022-12-11 17:54:09 +0530

    Java Number Lines Jump

--- day7.java
+++ day7.java
@@ -5,6 +5,7 @@ The first kangaroo starts at location x1 and moves at a rate of v1 meters per ju
 The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
 You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. If it is possible, return YES, otherwise return NO.
 
+Solution - 
 */
 
 import java.io.*;
