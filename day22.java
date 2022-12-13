34fe4c50c9c9d016fc1f97ca336ce238438dd133
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2022-12-13 20:19:33 +0530

    Java Apple and Orange

---
+++ day22.java
@@ -0,0 +1,131 @@
+/*
+Complete the countApplesAndOranges function in the editor below. It should print the number of apples and oranges that land on Sam's house, each on a separate line.
+
+countApplesAndOranges has the following parameter(s):
+
+s: integer, starting point of Sam's house location.
+t: integer, ending location of Sam's house location.
+a: integer, location of the Apple tree.
+b: integer, location of the Orange tree.
+apples: integer array, distances at which each apple falls from the tree.
+oranges: integer array, distances at which each orange falls from the tree.
+Input Format
+
+The first line contains two space-separated integers denoting the respective values of  and .
+The second line contains two space-separated integers denoting the respective values of  and .
+The third line contains two space-separated integers denoting the respective values of  and .
+The fourth line contains  space-separated integers denoting the respective distances that each apple falls from point .
+The fifth line contains  space-separated integers denoting the respective distances that each orange falls from point .
+
+Constraints
+
+Output Format
+
+Print two integers on two different lines:
+
+The first integer: the number of apples that fall on Sam's house.
+The second integer: the number of oranges that fall on Sam's house.
+Sample Input 0
+
+7 11
+5 15
+3 2
+-2 2 1
+5 -6
+Sample Output 0
+
+1
+1
+
+Solution - 
+
+*/
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'countApplesAndOranges' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER s
+     *  2. INTEGER t
+     *  3. INTEGER a
+     *  4. INTEGER b
+     *  5. INTEGER_ARRAY apples
+     *  6. INTEGER_ARRAY oranges
+     */
+
+    public static void countApplesAndOranges(int s, int t, int a, int b, List<Integer> apples, List<Integer> oranges) {
+    // Write your code here
+    int count_apple = 0;
+    int count_orange = 0;
+    
+    for(int i : apples){
+        if(i + a >= s && i + a <= t){
+            count_apple++;
+        }
+    }
+    
+    for(int i : oranges){
+        if(i + a >= s && i + a <= t){
+            count_orange++;
+        }
+    }
+    System.out.println(count_apple);
+    System.out.println(count_orange);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int s = Integer.parseInt(firstMultipleInput[0]);
+
+        int t = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] secondMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int a = Integer.parseInt(secondMultipleInput[0]);
+
+        int b = Integer.parseInt(secondMultipleInput[1]);
+
+        String[] thirdMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int m = Integer.parseInt(thirdMultipleInput[0]);
+
+        int n = Integer.parseInt(thirdMultipleInput[1]);
+
+        String[] applesTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> apples = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesTemp[i]);
+            apples.add(applesItem);
+        }
+
+        String[] orangesTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> oranges = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesTemp[i]);
+            oranges.add(orangesItem);
+        }
+
+        Result.countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        bufferedReader.close();
+    }
+}
