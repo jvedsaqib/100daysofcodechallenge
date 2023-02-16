45217237cd05b82c81d7bff32ce70a2a5d93b738
Saqib Javed <99673719+jvedsaqib@users.noreply.github.com>
2023-02-16 20:09:08 +0530

    Java Viral Advertising

---
+++ day87.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n) {
+    // Write your code here
+        List <Integer> days = new ArrayList<>();
+         for(int i = 1; i <= n; i++){
+             days.add(i);
+         }
+         
+         int shared = 5;
+         int comulative = 0;
+         
+         for (int i = 0; i < days.size(); i++){ 
+            shared = Math.floorDiv(shared,2);
+            comulative += shared;
+            shared = shared*3;
+         } 
+         
+         return comulative;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
